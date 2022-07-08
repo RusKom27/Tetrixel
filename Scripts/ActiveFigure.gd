c0083e26fe6fa23a62882eb30f65673c916f2c38
RusKom27 <48696676+RusKom27@users.noreply.github.com>
2022-07-08 20:15:22 +0300

    update

--- Scripts/ActiveFigure.gd
+++ Scripts/ActiveFigure.gd
@@ -1,7 +1,5 @@
 extends Node2D
 
-signal collision
-
 onready var game_board = get_parent().get_node("GameBoard")
 var resource_block = load("res://Components/Block.tscn")
 
@@ -11,16 +9,15 @@ var figure = [
 	[0, 1, 0],
 ]
 
-
 func _ready():
-	draw_figure()
+	change_figure()
 
 func _input(event):
 	if event.is_action_pressed("ui_left"):
 		figure_move(-16)
 	if event.is_action_pressed("ui_right"):
 		figure_move(16)
-	if event.is_action_pressed("ui_down"):
+	if event.is_action("ui_down"):
 		drop_down()
 	if event.is_action_pressed("ui_rotate_left"):
 		rotate_figure(-1)
@@ -37,7 +34,8 @@ func drop_down():
 	if collide():
 		position.y -= 16
 		merge()
-		position.y = game_board.position.y
+		game_board.matrix_sweep()
+		reset()
 
 func draw_figure():
 	for n in get_children():
@@ -45,7 +43,7 @@ func draw_figure():
 		n.queue_free()
 	for y in range(len(figure)):
 		for x in range(len(figure[y])):
-			if figure[y][x] == 1:
+			if figure[y][x] > 0:
 				var block = resource_block.instance()
 				block.position = Vector2(x*16, y*16)
 				add_child(block)
@@ -54,16 +52,32 @@ func collide():
 	var pos_in_matrix = get_position_in_matrix()
 	for y in range(len(figure)):
 		for x in range(len(figure[y])):
-			if figure[y][x] == 1:
+			if figure[y][x] > 0:
 				if y + pos_in_matrix.y > len(game_board.matrix) - 1:
 					return true
 				if x + pos_in_matrix.x < 0 or x + pos_in_matrix.x > len(game_board.matrix[0]) - 1:
 					return true
-				if game_board.matrix[y + pos_in_matrix.y][x + pos_in_matrix.x] == 1:
+				if game_board.matrix[y + pos_in_matrix.y][x + pos_in_matrix.x] > 0:
 					return true
 	return false
 
 func rotate_figure(dir):
+	var prev_position = position
+	var offset = Vector2(16,16)
+	rotate(dir)
+	while(collide()):
+		position.x += offset.x
+		if offset.x > 0:
+			offset.x = -(offset.x + 16)
+		else:
+			offset.x = -(offset.x - 16)
+		if offset.x > len(figure[0])*16:
+			rotate(-dir)
+			position.x = prev_position.x
+			return
+	draw_figure()
+
+func rotate(dir):
 	var N = len(figure[0])
 	if dir < 0:
 		for row in figure:
@@ -74,24 +88,79 @@ func rotate_figure(dir):
 			var temp = figure[i][j]
 			figure[i][j] = figure[j][i]
 			figure[j][i] = temp
-	
-	
 	for i in range(N):
 		for j in range(int(N/2)):
 			var temp = figure[i][j]
 			figure[i][j] = figure[i][N-j-1]
 			figure[i][N-j-1] = temp
-	draw_figure()
 	
 func merge():
 	for y in range(len(figure)):
 		for x in range(len(figure[y])):
-			if figure[y][x] == 1:
+			if figure[y][x] > 0 and \
+			 y + get_position_in_matrix().y < len(game_board.matrix) and \
+			 x + get_position_in_matrix().x < len(game_board.matrix[0]) and \
+			 x + get_position_in_matrix().x > -1:
 				game_board.matrix[y + get_position_in_matrix().y][x + get_position_in_matrix().x] = figure[y][x]
 	game_board.draw_matrix()
 
 func get_position_in_matrix():
 	return Vector2((position.x - game_board.position.x)/16,(position.y - game_board.position.y)/16) 
+
+func reset():
+	change_figure()
+	if collide():
+		game_board.clear_matrix()
+
+func change_figure():
+	var types = "ILJOTSZ"
+	figure = get_figure(types[randi()%len(types)])
+	draw_figure()
+	position = Vector2(game_board.position.x + 16*4, game_board.position.y + 16)
+
+func get_figure(type):
+	if type == 'I':
+		return [
+			[0, 1, 0, 0],
+			[0, 1, 0, 0],
+			[0, 1, 0, 0],
+			[0, 1, 0, 0],
+		]
+	elif type == 'L':
+		return [
+			[0, 2, 0],
+			[0, 2, 0],
+			[0, 2, 2],
+		]
+	elif type == 'J':
+		return [
+			[0, 3, 0],
+			[0, 3, 0],
+			[3, 3, 0],
+		]
+	elif type == 'O':
+		return [
+			[4, 4],
+			[4, 4],
+		]
+	elif type == 'Z':
+		return [
+			[5, 5, 0],
+			[0, 5, 5],
+			[0, 0, 0],
+		]
+	elif type == 'S':
+		return [
+			[0, 6, 6],
+			[6, 6, 0],
+			[0, 0, 0],
+		]
+	elif type == 'T':
+		return [
+			[0, 7, 0],
+			[7, 7, 7],
+			[0, 0, 0],
+		]
 
 func _on_Timer_timeout():
 	drop_down()
--- Scripts/GameBoard.gd
+++ Scripts/GameBoard.gd
@@ -12,15 +12,54 @@ func _ready():
 func create_matrix():
 	for y in range(height):
 		matrix.append([])
-		for x in range(width):
+		for _x in range(width):
 			matrix[y].append(0)
 
-func print_matrix(matrix):
-	print("______")
+func clear_matrix():
+	for y in range(height):
+		for x in range(width):
+			matrix[y][x] = 0
+	draw_matrix()
+
+func matrix_sweep():
+	print_matrix(matrix)
 	for y in range(len(matrix)):
-		var row = ""
+		var line_detected = true
 		for x in range(len(matrix[y])):
-			row += str(matrix[y][x]) + "|"
+			if matrix[y][x] == 0:
+				line_detected = false
+		if line_detected:
+			clear_row(y)
+	print_matrix(matrix)
+	draw_matrix()
+		
+		
+func clear_row(row):
+	for x in range(len(matrix[row])):
+		matrix[row][x] = 0
+
+	var rows = range(row)
+	rows.invert()
+	for y in rows:
+		var temp = matrix[y]
+		matrix[y] = matrix[y+1]
+		matrix[y+1] = temp
+
+func fill_row(row):
+	for y in range(row):
+		for x in range(len(matrix[0])):
+			matrix[len(matrix)-1-y][x] = 1
+	draw_matrix()
+
+func print_matrix(m):
+	var line = ""
+	for _x in range(len(m)):
+		line += "_"
+	print(line)
+	for y in range(len(m)):
+		var row = ""
+		for x in range(len(m[y])):
+			row += str(m[y][x]) + "|"
 		print(row)
 
 func draw_matrix():
@@ -29,7 +68,7 @@ func draw_matrix():
 		n.queue_free()
 	for y in range(len(matrix)):
 		for x in range(len(matrix[y])):
-			if matrix[y][x] == 1:
+			if matrix[y][x] > 0:
 				var block = resource_block.instance()
 				block.position = Vector2(x*16, y*16)
 				add_child(block)
